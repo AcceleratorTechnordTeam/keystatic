c891e08b716f99484cd120e19639ce7a338a6d09
AcceleratorTechnordTeam <n.chomier@technord.com>
2025-12-09 15:35:04 +0100

    ...

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -5,7 +5,7 @@ export const markdocConfig = fields.markdoc.createMarkdocConfig({});
 export default config({
 	storage: {
 		kind: 'github',
-		repo: 'AcceleratorTechnordTeam/keystatic',
+		repo: `${process.env.NEXT_PUBLIC_KEYSTATIC_GITHUB_REPO_OWNER}/${process.env.NEXT_PUBLIC_KEYSTATIC_GITHUB_REPO_NAME}`,
 	},
 	collections: {
 		posts: collection({
